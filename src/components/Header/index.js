48cf40f208745fd66f55a467659d478f10f55a6f
JaqueBRSilva <j.silvabroccolo@gmail.com>
2022-12-05 13:34:37 -0300

    feat: add componente 'Header'

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+
+const Header = ({ title }) => {
+    return (
+        <header className="App-header">
+            <h2>
+                { title }
+            </h2>
+        </header>
+    )
+}
+
+export default Header;
